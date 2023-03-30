5637f39b1642457938dd43e59ab0d94d40ca32e9
Sean Gombart <skgombart@gmail.com>
2023-03-29 23:38:24 -0700

    Skills page outlined

--- script.js
+++ script.js
@@ -6,28 +6,36 @@ const welcome = document.getElementById('welcome');
 const skills = document.getElementById('skills');
 const skillsSide = document.querySelector('.skills');
 const skillImgs = document.querySelectorAll('.skillImg');
+const skillImgTexts = document.querySelectorAll('.iconText');
+
 
 aboutSide.addEventListener('click', () => {
     cover.style.display = 'block';
     overlay.classList.toggle('overlay');
-})
+});
 
 skillsSide.addEventListener('click', () => {
     skills.style.display = 'block';
     overlay.classList.toggle('overlay');
-})
+});
 
 overlay.addEventListener('click', () => {
     cover.style.display = 'none';
     skills.style.display = 'none';
     overlay.classList.toggle('overlay');
-})
+});
 
-skillImgs.forEach(skillImg => {
+skillImgs.forEach((skillImg, index) => {
     skillImg.addEventListener('mouseover', () => {
-
+        skillImgTexts[index].style.display = 'block';
     })
+    skillImg.addEventListener('mouseout', () => {
+        skillImgTexts[index].style.display = 'none';
+      });
 })
+
+console.log('skillImg', skillImgs)
+console.log('texts', skillImgTexts)
 
 
 
